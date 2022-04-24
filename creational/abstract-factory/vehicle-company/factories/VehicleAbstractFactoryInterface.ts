8eb94c85891b918ba8180663be7125b60c6ec2bb
carlosdotdev <carlos10eduardo47@gmail.com>
2022-04-24 15:26:41 -0300

    fix: folder name refactor

--- creational/abstract-factory/vehicle-company/factories/VehicleAbstractFactoryInterface.ts
+++
@@ -1,9 +0,0 @@
-import CarInterface from "../products/cars/CarInterface";
-import MotorcycleInterface from "../products/motorcycles/MotorcycleInterface";
-
-interface VehicleAbstractFactoryInterface {
-  getCar(): CarInterface;
-  getMotorcycle(): MotorcycleInterface;
-}
-
-export default VehicleAbstractFactoryInterface;
